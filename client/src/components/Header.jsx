7c0ffb795d949082e12f10999f90909f655f299a
Avinash <iamavi2626@gmail.com>
2023-02-19 16:24:12 +0530

    changed favicon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,9 @@
-import logo from "../logo.svg";
 
 const Header = () => {
   return (
     <header className="header h-16 px-4 flex justify-between items-center text-white ">
       <a href="/" className="logo">
-          officeLaunge
+        officeLaunge
         </a>
       <div className="nav text-base" style={{ fontSize: "1.2rem", fontWeight: "bolder" }}>
         <a href="task" className="nav-item">
